fdd1a9692ad5cbf728994725f773b71befcb9753
Ivan Long <itec285@gmail.com>
2018-01-25 16:48:53 -0700

    Enhancements of app.py

--- app.py
+++ app.py
@@ -17,10 +17,22 @@ class StoreCodes_Meta(Resource):
 		#Connect to the database
 		conn = e.connect()
 		#perform query and return JSON data
-		query = conn.execute("Select distinct STORECODE from modules")
+		query = conn.execute("Select distinct STORECODE from Modules")
 		return {'StoreCodes': [i[0] for i in query.cursor.fetchall()]}
 
+class GetModules_Meta(Resource):
+	def get(self,store_code, external_IPAddress, internal_IPAddress):
+		#Connect to the database
+		conn = e.connect()
+		#Perform query and return JSON data
+		query = conn.execute("SELECT * FROM Modules WHERE StoreCode =?", (store_code.upper()))
+		return jsonify({'Data': query.cursor.fetchall()})
+		
+
+#The StoreCodes meta option is normally used for debugging only.
 api.add_resource(StoreCodes_Meta, '/starplus/api/v1.0/storecodes')
+api.add_resource(GetModules_Meta, '/starplus/api/v1.0/getmodules/<string:store_code>/<string:external_IPAddress>/<string:internal_IPAddress>')
+
 
 if __name__ == '__main__':
 	app.run()		
