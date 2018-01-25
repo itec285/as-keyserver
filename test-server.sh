b6a41b8ad364a382ee5c6f101a874cc65019fe83
Ivan Long <itec285@gmail.com>
2018-01-25 16:46:06 -0700

    Initial load from server

--- test-server.sh
+++ test-server.sh
@@ -1,1 +1,2 @@
-curl -i http://localhost:5000/starplus/api/v1.0/storecodes
+#curl -i http://localhost:5000/starplus/api/v1.0/storecodes
+curl -i http://localhost:5000/starplus/api/v1.0/getmodules/abcp01/24.244.1.123/10.10.1.1
